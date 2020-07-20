0aacd7d215f4ea386b57ea9ca13fe04a0d577a56
Roshan-Horo <35597955+Roshan-Horo@users.noreply.github.com>
2020-07-20 23:01:12 +0530

    Updates

---
+++ precache-manifest.6c4aab1b41ea110cab604d453c2d9d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2259774231004ff57b0abf1439242f0",
+    "url": "/card-app/index.html"
+  },
+  {
+    "revision": "ee7c2132837ea3607436",
+    "url": "/card-app/static/css/main.e410e593.chunk.css"
+  },
+  {
+    "revision": "d352e17e89f2cf99fca9",
+    "url": "/card-app/static/js/2.9ea00ab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-app/static/js/2.9ea00ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7c2132837ea3607436",
+    "url": "/card-app/static/js/main.358c64e7.chunk.js"
+  },
+  {
+    "revision": "d53925a899eff0cfd010",
+    "url": "/card-app/static/js/runtime-main.d35341a5.js"
+  }
+]);
